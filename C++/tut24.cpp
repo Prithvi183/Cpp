9c8403e57384fb23975f41c92b889ff354e68c41
Prithviraj <prithvi.patil183@gmail.com>
2022-01-21 23:38:40 +0530

    Modified tut24

--- C++/tut24.cpp
+++ C++/tut24.cpp
@@ -1,8 +1,29 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main()
+class Employee
 {
-    
+    int id;
+    int count;
+    public:
+    void setData(void){
+        cout<<"Enter the id: ";
+        cin>>id;
+    }
+    void getData(void){
+        cout<<"The id of this employee is "<<id<<endl;
+    }
+};
+
+int
+main()
+{
+    Employee prithvi;
+    // prithvi.id = 1;
+    // prithvi.count = 1; // Cannot do this cause id and count are private.
+
+    prithvi.setData();
+    prithvi.getData();
+
     return 0;
 }
