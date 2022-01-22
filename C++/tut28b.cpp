2031e390a81e88ad302fd108f58cd87e6c3d8c5f
Prithviraj <prithvi.patil183@gmail.com>
2022-01-22 16:36:58 +0530

    Modified tut28b

--- C++/tut28b.cpp
+++ C++/tut28b.cpp
@@ -1,36 +1,45 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 class c2;
 
-class c1{
+class c1
+{
     int val1;
     friend void exchange(c1 &, c2 &);
-    public:
-        void inData(int a){
-            val1 = a;
-        }
-        void display(void){
-            cout<< val1 << endl;
-        }
+
+public:
+    void inData(int a)
+    {
+        val1 = a;
+    }
+    void display(void)
+    {
+        cout << val1 << endl;
+    }
 };
 
-class c2{
+class c2
+{
     int val2;
     friend void exchange(c1 &, c2 &);
-    public:
-        void inData(int a){
-            val2 = a;
-        }
-        void display(void){
-            cout<< val2 << endl;
-        }
+
+public:
+    void inData(int a)
+    {
+        val2 = a;
+    }
+    void display(void)
+    {
+        cout << val2 << endl;
+    }
 };
 
-void exchange(c1 &x, c2 &y){
-        int tmp = x.val1;
-        x.val1 = y.val2;
-        y.val2 = tmp;
+void exchange(c1 &x, c2 &y)
+{
+    int tmp = x.val1;
+    x.val1 = y.val2;
+    y.val2 = tmp;
 }
 
 int main()
@@ -42,9 +51,9 @@ int main()
     oc2.inData(64);
     exchange(oc1, oc2);
 
-    cout<<"The value of c1 after exchanging becomes: ";
-    oc1.display(); 
-    cout<<"The value of c2 after exchanging becomes: ";
-    oc2.display(); 
+    cout << "The value of c1 after exchanging becomes: ";
+    oc1.display();
+    cout << "The value of c2 after exchanging becomes: ";
+    oc2.display();
     return 0;
 }
