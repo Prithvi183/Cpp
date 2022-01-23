e8795e3bfbf089b9e525c007f8d8c700928d86d9
Prithviraj <prithvi.patil183@gmail.com>
2022-01-23 15:24:04 +0530

    Modified tut32

--- C++/tut32.cpp
+++ C++/tut32.cpp
@@ -1,8 +1,24 @@
 #include<iostream>
 using namespace std;
 
+class Simple{
+    int data1;
+    int data2;
+    public:
+        Simple(int a, int b=9){  //b is given a default argument.
+            data1 = a;
+            data2 = b;
+        }
+        void printData();
+};
+
+void Simple :: printData(){
+    cout<<"The value of the data1 and data2 is "<<data1<<" and "<<data2<<endl;
+}
+
 int main()
 {
-    
+    Simple s(1);
+    s.printData();
     return 0;
 }
