1545a2ad33e98c4697a3830dcafe1ca43e2f640e
Prithviraj <prithvi.patil183@gmail.com>
2022-01-27 15:23:37 +0530

    Modified tut39

--- C++/tut39.cpp
+++ C++/tut39.cpp
@@ -2,15 +2,28 @@
 using namespace std;
 
 class Base{
-
+    protected:
+        int a;
+    private:
+        int b;
 };
+/*
+For a protected member:
+                                Public Derivation   Private derivation   Protected derivation
+        1. Private members.     Not Inherited       Not Inherited        Not Inherited   
+        2. Protected members.   Private             Private              Protected
+        3. Public members.      Public              Private              Protected
+*/
 
-class Derived : Base{
+class Derived : protected Base{
 
 };
 
 int main()
 {
-    
+    Base b;
+    Derived d;
+    // cout<<b.a<<endl;  // Will not work since 'a' is protected in both base as well as derived class.
+    // cout<<d.a<<endl;  // Will not work since 'a' is protected in both base as well as derived class.
     return 0;
 }
