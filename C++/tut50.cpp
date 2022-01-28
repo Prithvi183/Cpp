ba515fcd0b6cdcdcf998aa61d9f9a0e599e471ab
Prithviraj <prithvi.patil183@gmail.com>
2022-01-29 01:49:49 +0530

    Modified tut50

--- C++/tut50.cpp
+++ C++/tut50.cpp
@@ -3,6 +3,26 @@ using namespace std;
 
 int main()
 {
-    
+    // Basic Example:
+    int a = 4;
+    int* ptr = &a;
+
+    cout<<"The value of a is "<<*(ptr)<<endl;
+
+    // New operator
+    int* p = new int(10);
+    cout<<"The value at address p is "<<*(p)<<endl;
+
+    int* arr = new int[3];
+    arr[0]=10;
+    *(arr+1)=20;  // <--> Also written this way
+    arr[2]=30;
+    // delete[] arr;
+    cout<<"The value at address arr[0] is "<<arr[0]<<endl;
+    cout<<"The value at address arr[1] is "<<arr[1]<<endl;
+    cout<<"The value at address arr[2] is "<<arr[2]<<endl;
+
+    // Delete operator
+
     return 0;
 }
