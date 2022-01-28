ee78688cc69545c974a668aa92d1548d7974f081
Prithviraj <prithvi.patil183@gmail.com>
2022-01-28 16:47:25 +0530

    Modified tut42

--- C++/tut42.cpp
+++ C++/tut42.cpp
@@ -1,8 +1,17 @@
 #include<iostream>
 using namespace std;
 
-class
+/*
+Create 2 class:
+    1. Simple Calculator - Takes input of two numbers using utility function and perform +,-,*,/.
+    1. Scientific Calculator -  Takes input of two numbers using utility function and perform any 4 scientific operations of your choice.
 
+Create another class HybridCalculator and inherit it using above two classes:
+    Q1. What type of inheritance is used?
+    Q2. Which mode of iheritance are you using?
+    Q3. Create an object of HybridCalculator and display results of simple and scientific calculators?
+    Q4. How is code reusability implemented?
+*/
 int main()
 {
     
