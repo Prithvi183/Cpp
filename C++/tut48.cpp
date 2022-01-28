6bea27199d6acb6856fdf4065fe02a4b5cd02570
Prithviraj <prithvi.patil183@gmail.com>
2022-01-28 23:35:07 +0530

    Modified tut48

--- C++/tut48.cpp
+++ C++/tut48.cpp
@@ -1,8 +1,37 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
+
+/*
+Case 1:
+class B: public A{
+    Order of execution of constructor -> first A() then B()
+};
+
+Case 2:
+class A: public B, public C{
+    Order of execution of constructor -> B() then C() and then A()
+};
+
+Case 3:
+class A: public B, virtual public C{
+    Order of execution of constructor -> C() then B() and then A()
+};
+*/
+
+class Base
+{
+    int data;
+
+public:
+    Base(int i)
+    {
+        data = i;
+        cout << "Base class constructor is called." << endl;
+    }
+};
 
 int main()
 {
-    
+
     return 0;
 }
