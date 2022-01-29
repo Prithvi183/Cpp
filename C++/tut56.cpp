f2fc1d5c60b4da80892edbe777bc7a762ef4ccff
Prithviraj <prithvi.patil183@gmail.com>
2022-01-29 19:39:39 +0530

    Modified tut56

--- C++/tut56.cpp
+++ C++/tut56.cpp
@@ -1,8 +1,33 @@
 #include<iostream>
 using namespace std;
 
+class BaseClass
+{
+public:
+    int var_base = 1;
+    virtual void display()
+    {
+        cout << "Displaying Base Class variable var_base " << var_base << endl;
+    };
+};
+
+class DerivedClass : public BaseClass
+{
+public:
+    int var_derived = 2;
+    void display()
+    {
+        cout << "Displaying Derived Class variable var_derived " << var_derived << endl;
+    };
+};
+
 int main()
 {
-    
+    BaseClass *bptr;
+    BaseClass obj_base;
+    DerivedClass obj_derived;
+
+    bptr = &obj_derived;
+    bptr->display();
     return 0;
 }
