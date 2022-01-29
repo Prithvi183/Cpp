c4353642725bf97b91de1f5b2d69b79012a0519a
Prithviraj <prithvi.patil183@gmail.com>
2022-01-29 16:46:34 +0530

    "Modified tut53"

--- C++/tut53.cpp
+++ C++/tut53.cpp
@@ -1,8 +1,28 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
+
+class A
+{
+    int a;
+
+public:
+    // A & setData(int a)
+    void setData(int a)
+    {
+        this->a = a;
+        // return *this;
+    }
+    void getData()
+    {
+        cout << "The value of a is " << a << endl;
+    }
+};
 
 int main()
 {
-    
+    // "this" is a keyword which is a pointer which points to the object which invokes the member function.
+    A a;
+    a.setData(6);
+    a.getData();
     return 0;
 }
