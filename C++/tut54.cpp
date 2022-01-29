38fd03d6188bd4ca9587f9e544dc1379d4842005
Prithviraj <prithvi.patil183@gmail.com>
2022-01-29 17:11:36 +0530

    Modified tut54

--- C++/tut54.cpp
+++ C++/tut54.cpp
@@ -3,6 +3,14 @@ using namespace std;
 
 int main()
 {
-    
+    /*
+    Polymorphism
+        - One name and multiple forms.
+        - Eg. Function overloading, operator overloading, Virtual functions
+        - 1. Complile Time polymorphism:
+            eg. Function overloading, Operator overloading.
+        - 2. Run Time polymorphism:
+            eg. Virtual function.
+    */
     return 0;
 }
