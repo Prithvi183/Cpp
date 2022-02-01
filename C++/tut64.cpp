a9429fa62925e6e0d53a95a3dfc1213d0306aaca
Prithviraj <prithvi.patil183@gmail.com>
2022-02-02 02:09:56 +0530

    Modified tut64

--- C++/tut64.cpp
+++ C++/tut64.cpp
@@ -1,8 +1,55 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
+
+template <class T>
+
+class vector
+{
+
+public:
+    T *arr;
+    int size;
+    vector(int m)
+    {
+        size = m;
+        arr = new T[size];
+    }
+
+    T dotProduct(vector &v)
+    {
+        T d = 0;
+        for (int i = 0; i < size; i++)
+        {
+            d += this->arr[i] * v. arr[i];
+        }
+            return d;
+    }
+};
 
 int main()
 {
+    // vector <int> v1(3);
+    // v1.arr[0] = 4;
+    // v1.arr[1] = 8;
+    // v1.arr[2] = 5;
+
+    // vector <int> v2(3);
+    // v2.arr[0] = 04;
+    // v2.arr[1] = 5;
+    // v2.arr[2] = 3;
     
+    vector <float> v1(3);
+    v1.arr[0] = 0.4;
+    v1.arr[1] = 0.8;
+    v1.arr[2] = 1.5;
+
+    vector <float> v2(3);
+    v2.arr[0] = 0.4;
+    v2.arr[1] = 0.5;
+    v2.arr[2] = 0.3;
+
+    int a = v1.dotProduct(v2);
+    cout << a << endl;
+
     return 0;
 }
