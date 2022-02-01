007b4f4b5b4d6e14c545186274d4bd23f12fa1ed
Prithviraj <prithvi.patil183@gmail.com>
2022-02-01 18:59:19 +0530

    Modified tut61

--- C++/tut61.cpp
+++ C++/tut61.cpp
@@ -1,8 +1,28 @@
 #include<iostream>
+#include<fstream> 
+
 using namespace std;
 
 int main()
 {
-    
+    // Connecting our file with out stream
+    ofstream out("sample60.txt");
+
+    // Creating a name variable entered by a user
+    string name;
+    cout<<"Enter your name: ";
+    cin>>name;
+
+    // Writing a string to the file.2
+    out<<"My name is " + name;
+    out.close();
+
+    ifstream in("sample60.txt");
+    string content;
+    in>>content;
+    getline(in, content);
+    cout<<"The content of the file is: "<<content;
+    in.close();
+
     return 0;
 }
