f4c538f9d3e8f18eba9d275ccb420627e6a6af42
Prithviraj <prithvi.patil183@gmail.com>
2022-02-04 20:20:04 +0530

    Modified tut72

--- C++/tempCodeRunnerFile.cpp
+++ C++/tempCodeRunnerFile.cpp
@@ -1,5 +1,2 @@
-template<class t1, class t2>
-// float funcAvg2(t1 a, t2 b){
-//     float avg = (a+b)/2;
-//     return avg;
-// }
+iter++;
+    cout<<*iter<<endl;
--- C++/tut72.cpp
+++ C++/tut72.cpp
@@ -2,20 +2,53 @@
 #include<list>
 using namespace std;
 
+    void display(list<int> &lst){
+        list<int> :: iterator it;
+        for (it = lst.begin(); it != lst.end(); it++)
+        {
+                cout<<*it<<" ";
+        }
+        cout<<endl;
+    };
+
 int main()
 {
     list<int> list1; // List of 0 length.
-    // list<int> list2(7); // Empty list of size 7.
     list1.push_back(5);
     list1.push_back(4);
     list1.push_back(8);
     list1.push_back(9);
+    list1.push_back(9);
     list1.push_back(12);
+    display(list1);
+    list1.pop_front();
+    display(list1);
+    list1.remove(9);
+    display(list1);
 
+    // Sorting a list.
+    list1.sort();
+    display(list1);
+
+    // Reversing a list
+    list1.reverse();
+    display(list1);
+
+    list<int> list2(3); // Empty list of size 7.
     list<int> :: iterator iter;
-    iter = list1.begin();
-    cout<<*iter<<endl;
+    iter = list2.begin();
+    *iter = 45;
     iter++;
-    cout<<*iter<<endl;
+    *iter = 5;
+    iter++;
+    *iter = 4;
+    iter++;
+    display(list2);
+    list1.sort();
+    list2.sort();
+    cout<<"List1 after merging: "<<endl;
+    list1.merge(list2);
+    display(list1);
+
     return 0;
 }
