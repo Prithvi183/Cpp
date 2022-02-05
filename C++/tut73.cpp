69311545da5ef51b0d0fbffb29b46bcafd2b26e8
Prithviraj <prithvi.patil183@gmail.com>
2022-02-05 16:33:09 +0530

    Modified tut73

--- C++/tut73.cpp
+++ C++/tut73.cpp
@@ -1,8 +1,23 @@
 #include<iostream>
+#include<string>
+#include<map>
 using namespace std;
+
+// Map is an associative array.
 
 int main()
 {
+    map <string, int> marksMap;
+    marksMap["prithvi"] = 98;
+    marksMap["rahul"] = 88;
+    marksMap["rohan"] = 78;
+
+    map <string, int> :: iterator iter;
+    for (iter = marksMap.begin(); iter != marksMap.end(); iter++)
+    {
+        /* code */
+    }
     
+
     return 0;
 }
