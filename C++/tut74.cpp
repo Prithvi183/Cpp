c8f0c9ea25ebe2e3858c759d14802b6ba78cd1f4
Prithviraj <prithvi.patil183@gmail.com>
2022-02-05 17:28:56 +0530

    Modified tut74

--- C++/tut74.cpp
+++ C++/tut74.cpp
@@ -1,8 +1,18 @@
 #include<iostream>
+#include<functional>
+#include<algorithm>
 using namespace std;
 
 int main()
 {
-    
+    // Function objects (Functor): Function wrapped in a class so that it can be available like an object.
+    int arr[] = {1, 24, 3, 4, 68, 2};
+    sort(arr, arr+5);
+    sort(arr, arr+6, greater<int>());
+    for (int i = 0; i < 6; i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+
     return 0;
 }
