14ee1aea12375434ed8af3ba30e499f9709030d3
Prithviraj <prithvi.patil183@gmail.com>
2022-04-16 19:16:11 +0530

    Modified tut4

--- C++/tut4.cpp
+++ C++/tut4.cpp
@@ -20,4 +20,39 @@ int main()
     return 0;
 }
 
-// Note:   Local variable have higher precedence than global variable.
+/* Note:   Local variable have higher precedence than global variable.
+
+Local variables:
+Local variables are declared inside the braces of any function and can be assessed only from there. 
+
+Global variables:
+Global variables are declared outside any function and can be accessed from anywhere.
+
+Data Types:
+
+1) Built-in Data-types:
+    Int
+    Float
+    Char
+    Double
+    Boolean
+
+2) User-Defined Data-types:
+    struct
+    Union
+    Enum
+
+3) Derived Data-types:
+    Arrays
+    Pointers
+    Functions
+
+Rules to Declare Variables in C++
+    Variable names in C++ language can range from 1 to 255
+    Variable names must start with a letter of the alphabet or an underscore
+    After the first letter, variable names can contain letters and numbers
+    Variable names are case sensitive
+    No spaces and special characters are allowed
+    We cannot use reserved keywords as a variable name
+
+*/
