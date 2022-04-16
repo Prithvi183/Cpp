5c7d0505a3c980802ee4875b3426a1843f76cbb7
Prithviraj <prithvi.patil183@gmail.com>
2022-04-16 17:26:49 +0530

    Modified tut4

--- C++/tut4.cpp
+++ C++/tut4.cpp
@@ -1,18 +1,20 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 int main()
 {
-    int a = 3;  //Variable1
-    int b = 2;  //Variable2
+    int a = 3; // Variable1
+    int b = 2; // Variable2
     float pi = 3.14;
     char c = 'p';
     bool is_true = true;
-    cout<<"This is tutorial 4."<<endl<< "The value of a is "<<a<<endl<<"The value of b is "<<b<<endl;
-    cout<<"The value of pi is "<<pi<<endl;
-    cout<<"The character is "<<c<<endl;
-    cout<<"The value for true is "<<is_true<<endl;
+    cout << "This is tutorial 4." << endl
+         << "The value of a is " << a << endl
+         << "The value of b is " << b << endl;
+    cout << "The value of pi is " << pi << endl;
+    cout << "The character is " << c << endl;
+    cout << "The value for true is " << is_true << endl;
     return 0;
 }
 
-//Note:   Local variable has higher precedence than global variable.
+// Note:   Local variable have higher precedence than global variable.
