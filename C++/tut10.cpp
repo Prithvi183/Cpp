9bdeb3c208cfd89ad4e8da702dce3bab6ea5155b
Prithviraj <prithvi.patil183@gmail.com>
2022-04-17 17:17:02 +0530

    Modified tut10

--- C++/tut10.cpp
+++ C++/tut10.cpp
@@ -1,37 +1,39 @@
-#include<iostream>
+// For, While and do-while loops in C++
+
+#include <iostream>
 using namespace std;
 
 int main()
 {
     /* Loops in C++
     There are Three types of Loops in C++:
-        1. For Loop
+        1.For Loop
         2.While Loop
-        3.Do-While Loop 
+        3.Do-While Loop
     */
 
     // For Loop in C++.
 
-    //syntax of for loop
+    // syntax of for loop
     /* for(initialization; condition; updation)
     {
         Loop body(C++ code)
     }
     */
-    
+
     // for (int i = 0; i < 20; i++)
     // {
     //     /* code */
     //     cout<<i<<endl;
     // }
-    
-    //While Loop in C++.
 
-    //syntax of While loop
+    // While Loop in C++.
+
+    // syntax of While loop
     /* while (conditions)
     {
         C++ statements;
-    } 
+    }
     */
 
     // int i=1;
@@ -41,10 +43,10 @@ int main()
     //     cout<<i<<endl;
     //     i++;
     // }
-    
-    //Do-While loop in C++.
 
-    //syntax of do-while loop
+    // Do-While loop in C++.
+
+    // syntax of do-while loop
     /*
     do
     {
@@ -53,13 +55,13 @@ int main()
     while (condition)
     */
 
-    int i=1;
+    int i = 1;
     do
     {
         /* code */
-        cout<<i<<endl;
+        cout << i << endl;
         i++;
-    } while (i<=40);
-    
+    } while (i <= 40);
+
     return 0;
 }
