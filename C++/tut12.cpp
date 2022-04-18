bda835c4ab19fa8e050c5633bd648a2c55972cd1
Prithviraj <prithvi.patil183@gmail.com>
2022-04-18 21:55:19 +0530

    Modified tut12

--- C++/tut12.cpp
+++ C++/tut12.cpp
@@ -1,27 +1,31 @@
-#include<iostream>
+// Pointers in C++
+
+#include <iostream>
 using namespace std;
 
 int main()
 {
-    //What is a pointer? -----> Data type which holds the address of the other data types.
+    // What is a pointer? -----> A pointer is a data type which holds the address of other data type. The “&” operator is called “address of" operator, and the "*” operator is called “value at” dereference operator
 
     int a = 3;
-    int* b;
+    int *b;
     b = &a;
 
     // & -----> (Address of) operator
-    cout<< "The address of a is "<<&a<<endl;
-    cout<< "The address of a is "<<b<<endl;
+    cout << "The address of a is " << &a << endl;
+    cout << "The address of a is " << b << endl;
 
     // * -----> (Value at) Dereference operator
-    cout<< "The value at address b is "<<*b<<endl;
+    cout << "The value at address b is " << *b << endl;
 
-    //Pointer to pointer
-    int** c = &b;
-    cout<< "The address of b is "<<&b<<endl;
-    cout<< "The address of b is "<<c<<endl;
-    cout<< "The value at address b is "<<*c<<endl;
-    cout<< "The value at address(value_at(c)) is "<<**c<<endl;
+    // Pointer to pointer
+    // int** c = &b;
+    int** c;
+    c = &b;
+    cout << "The address of b is " << &b << endl;
+    cout << "The address of b is " << c << endl;
+    cout << "The value at address b is " << *c << endl;
+    cout << "The value at address(value_at(c)) is " << **c << endl;
 
     return 0;
 }
